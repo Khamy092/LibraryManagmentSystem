4f85bc03ca4776be1ad465edeca4d42066debb97
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-22 16:37:35 +0930

    minor changes in methods

--- Library.py
+++ Library.py
@@ -7,9 +7,13 @@ class Library:
         self._transactions = []
 
     def add_book(self, book):
+        if book.isbn in [book.isbn for book in self._books]:
+            raise ValueError("Book with ISBN {} already exists".format(book.isbn))
         self._books.append(book)
-
+    
     def remove_book(self, book):
+        if self.isBookBorrowed(book):
+            raise ValueError("Book is borrowed")
         self._books.remove(book)
 
     def add_patron(self, patron):
@@ -30,3 +34,8 @@ class Library:
     def get_patrons(self):
         return self._patrons
     
+    def isBookBorrowed(self, book):
+        for transaction in self._transactions:
+            if transaction.book == book and not transaction.returned:
+                return True
+        return False
