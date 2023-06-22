0f337e8299a560a7ff596b71125031731069743e
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-22 16:01:35 +0930

    Book getters and setters are added and empy methods created.

--- Book.py
+++ Book.py
@@ -1,1 +1,66 @@
 # Book class
+
+
+class Book:
+    def __init__(self, title, author, year, isbn, NumberOfCopies=0):
+        self._title = title
+        self._author = author
+        self._year = year
+        self._isbn = isbn
+        self.isAvailable = True
+        self.numberOfCopies = NumberOfCopies
+
+    
+    # Getters and setters using the @property decorator
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if title == "":
+            raise ValueError("Title cannot be empty.")
+        self._title = title
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if author == "":
+            raise ValueError("Author cannot be empty.")
+        self._author = author
+
+    @property
+    def year(self):
+        return self._year
+    
+    @year.setter
+    def year(self, year):
+        if year == "" or type(year) != int:
+            raise ValueError("Year cannot be empty and must be a number.")
+        self._year = year
+
+    @property
+    def isbn(self):
+        return self._isbn
+    
+    @isbn.setter
+    def isbn(self, isbn):
+        if isbn == "":
+            raise ValueError("ISBN cannot be empty.")
+        self._isbn = isbn
+
+    
+    # Methods
+
+    def check_availability(self):
+        pass
+
+    def update_availability(self):
+        pass
+
+    def display_info(self):
+        pass
--- Library.py
+++ Library.py
@@ -1,2 +1,32 @@
 # Library main file
 
+class Library:
+    def __init__(self):
+        self._books = []
+        self._patrons = []
+        self._transactions = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def remove_book(self, book):
+        self._books.remove(book)
+
+    def add_patron(self, patron):
+        self._patrons.append(patron)
+    
+    def remove_patron(self, patron):
+        self._patrons.remove(patron)
+    
+    def add_transaction(self, transaction):
+        self._transactions.append(transaction)
+
+    def remove_transaction(self, transaction):
+        self._transactions.remove(transaction)
+    
+    def get_books(self):
+        return self._books
+    
+    def get_patrons(self):
+        return self._patrons
+    
